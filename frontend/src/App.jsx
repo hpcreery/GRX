46d33c42569e2f447330fe1ef0f771ae6afb44f6
Hunter <hpcreery@gmail.com>
2020-09-28 00:04:26 -0500

    Easy to use production and dev env variables

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,57 +1,52 @@
-import React, { Component } from 'react';
-import { Spin, Switch, Alert, Button, Card } from 'antd';
-import { LoadingOutlined } from '@ant-design/icons';
-import './App.css';
-import Renderer from './components/Renderer';
+import React, { Component } from 'react'
+import { Spin, Switch, Alert, Button, Card } from 'antd'
+import { LoadingOutlined } from '@ant-design/icons'
+import './App.css'
+import Renderer from './components/Renderer'
 
 //const fs = window.require('fs');
-const pcbStackup = require('pcb-stackup');
+const pcbStackup = require('pcb-stackup')
 //require('./components/ThreeRenderer')
 //require('./dev/main')
 
-var parser = new DOMParser();
+var parser = new DOMParser()
 
 class App extends Component {
   constructor(props) {
-    super(props);
-    this.state = { windowheight: window.innerHeight };
+    super(props)
+    this.state = { windowheight: window.innerHeight }
   }
 
   componentDidMount() {
-    this.elements = document.getElementById('elements');
-    window.addEventListener('resize', this.onWindowResize);
+    this.elements = document.getElementById('elements')
+    window.addEventListener('resize', this.onWindowResize)
+    console.log(process.env)
   }
 
   onWindowResize = () => {
-    //console.log(this.elements)
-    this.setState({ windowheight: window.innerHeight });
-    //this.elements.style.height = `${window.innerHeight}`
-  };
+    this.setState({ windowheight: window.innerHeight })
+  }
 
   render() {
-    console.log('Rendering App');
+    console.log('Rendering App')
     return (
-      <div
-        className="elements"
-        style={{ height: this.state.windowheight }}
-        id="elements"
-      >
+      <div className='elements' style={{ height: this.state.windowheight }} id='elements'>
         <Renderer />
         {/* <Card title='test' className='sidebar'>
           <Button onClick={() => this.testFetch()}>Hello</Button>
         </Card> */}
-        <div className="icon-attribute">
+        <div className='icon-attribute'>
           <h6>!! DEVELOPMENT !!</h6>
           <h6>
             Software by{' '}
-            <a target="_blank" href="https://github.com/hpcreery">
+            <a target='_blank' href='https://github.com/hpcreery'>
               Hunter Creery
             </a>{' '}
           </h6>
         </div>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
--- frontend/src/config/config.js
+++ frontend/src/config/config.js
@@ -2,6 +2,6 @@ const dotenv = require('dotenv')
 dotenv.config()
 
 module.exports = {
-  backendurl: process.env.API || 'http://172.20.7.68',
-  port: process.env.APIPORT || ':8081',
+  backendurl: process.env.REACT_APP_API || 'http://grx.creery.org',
+  port: process.env.REACT_APP_APIPORT || '/api',
 }
