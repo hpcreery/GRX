ee9de501d4a6de704d7237316d19309523f4a6e1
Hunter <hpcreery@gmail.com>
2020-09-28 00:27:11 -0500

    And another one
    I need to find a better way about this.....

--- frontend/src/config/config.js
+++ frontend/src/config/config.js
@@ -2,6 +2,6 @@
 // dotenv.config({ path: `${__dirname}/../../../.env.development` })
 
 module.exports = {
-  backendurl: process.env.REACT_APP_API || 'http://grx.creery.org',
+  backendurl: process.env.REACT_APP_API || 'https://grx.creery.org',
   port: process.env.REACT_APP_APIPORT || '/api',
 }
