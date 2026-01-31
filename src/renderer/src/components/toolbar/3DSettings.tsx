49038e342c1674f3ebea9341065b939fcc60d6cc
Hunter <hpcreery@gmail.com>
2026-01-31 13:15:10 -0600

    refactor(kbd): change keyboard shortcuts a little for 3D modes

--- src/renderer/src/components/toolbar/3DSettings.tsx
+++ src/renderer/src/components/toolbar/3DSettings.tsx
@@ -6,28 +6,26 @@ import { actions } from "@src/contexts/Spotlight"
 import { EditorConfigProvider } from "@src/contexts/EditorContext"
 
 interface ThreeDSettingsProps {}
-const ViewModes = ['Off', '3D Orthographic', '3D Perspective'] as const;
-type ViewMode = typeof ViewModes[number];
+const ViewModes = ["Off", "3D Orthographic", "3D Perspective"] as const
+type ViewMode = (typeof ViewModes)[number]
 
 export default function ThreeDSettings(_props: ThreeDSettingsProps): JSX.Element | null {
   const { renderer } = useContext(EditorConfigProvider)
   const [mode, setMode] = useLocalStorage<ViewMode>({
     key: "engine:3D_MODE",
-    defaultValue: 'Off',
+    defaultValue: "Off",
   })
 
   async function getModes(): Promise<void> {
-    const mode = await renderer.engine.interface.read_engine_settings().then(settings => settings.ENABLE_3D)
-    // setEnable3D(mode)
-    const pMode = await renderer.engine.interface.read_engine_settings().then(settings => settings.PERSPECTIVE_3D)
-    // setPerspective3D(pMode)
+    const mode = await renderer.engine.interface.read_engine_settings().then((settings) => settings.ENABLE_3D)
+    const pMode = await renderer.engine.interface.read_engine_settings().then((settings) => settings.PERSPECTIVE_3D)
     if (!mode) {
-      setMode('Off')
+      setMode("Off")
     } else {
       if (pMode) {
-        setMode('3D Perspective')
+        setMode("3D Perspective")
       } else {
-        setMode('3D Orthographic')
+        setMode("3D Orthographic")
       }
     }
   }
@@ -37,42 +35,65 @@ export default function ThreeDSettings(_props: ThreeDSettingsProps): JSX.Element
   }, [])
 
   useEffect(() => {
-    const enable3D = mode !== 'Off'
-    const perspective3D = mode === '3D Perspective'
+    const enable3D = mode !== "Off"
+    const perspective3D = mode === "3D Perspective"
     renderer.engine.interface.set_engine_settings({ ENABLE_3D: enable3D })
     renderer.engine.interface.set_engine_settings({ PERSPECTIVE_3D: perspective3D })
   }, [mode])
 
-  function cycleMode(): ViewMode {
-    if (mode === 'Off') {
-      return '3D Orthographic'
-    } else if (mode === '3D Orthographic') {
-      return '3D Perspective'
-    } else {
-      return 'Off'
-    }
-  }
+  // const cycleMode = (): ViewMode => {
+  //   if (mode === 'Off') {
+  //     return '3D Orthographic'
+  //   } else if (mode === '3D Orthographic') {
+  //     return '3D Perspective'
+  //   } else {
+  //     return 'Off'
+  //   }
+  // }
 
   useEffect(() => {
     actions.push({
-      id: "toggle 3D mode",
-      label: "Toggle 3D Mode",
-      description: "Cycle through 3D view modes",
-      onClick: () => setMode(cycleMode()),
+      id: "toggle 3D orthographic mode",
+      label: "Toggle 3D Orthographic Mode",
+      description: "Switch to 3D Orthographic view",
+      onClick: () => setMode("3D Orthographic"),
       leftSection: <IconBadge3d />,
       rightSection: (
         <div dir="ltr">
-          <Kbd>T</Kbd>
+          <Kbd>Alt</Kbd> + <Kbd>O</Kbd>
+        </div>
+      ),
+    })
+    actions.push({
+      id: "toggle 3D perspective mode",
+      label: "Toggle 3D Perspective Mode",
+      description: "Switch to 3D perspective view",
+      onClick: () => setMode("3D Perspective"),
+      leftSection: <IconBadge3d />,
+      rightSection: (
+        <div dir="ltr">
+          <Kbd>Alt</Kbd> + <Kbd>P</Kbd>
+        </div>
+      ),
+    })
+    actions.push({
+      id: "toggle 3D off mode",
+      label: "Toggle 3D Mode Off",
+      description: "Turn off 3D view",
+      onClick: () => setMode("Off"),
+      leftSection: <IconBadge3d />,
+      rightSection: (
+        <div dir="ltr">
+          <Kbd>Alt</Kbd> + <Kbd>I</Kbd>
         </div>
       ),
     })
   }, [])
 
   useHotkeys([
-    [
-      "t",
-      (): void => setMode(cycleMode()),
-    ],
+    ["alt+o", (): void => setMode("3D Orthographic"), { usePhysicalKeys: true, preventDefault: true }],
+    ["alt+p", (): void => setMode("3D Perspective"), { usePhysicalKeys: true, preventDefault: true }],
+    ["alt+i", (): void => setMode("Off"), { usePhysicalKeys: true, preventDefault: true }],
   ])
 
   return <SegmentedControl radius="sm" value={mode} data={ViewModes.slice()} onChange={(val) => val && setMode(val as ViewMode)} />
