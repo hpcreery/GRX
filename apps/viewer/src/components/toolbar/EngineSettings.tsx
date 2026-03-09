f581f2f1febc30e8136f5e6aba237bfcf67e877d
Hunter <hpcreery@gmail.com>
2026-03-08 21:12:04 -0500

    fix: changing any engine settings altered 3d mode unintentionally in ui

--- apps/viewer/src/components/toolbar/EngineSettings.tsx
+++ apps/viewer/src/components/toolbar/EngineSettings.tsx
@@ -22,14 +22,6 @@ export default function EngineSettings(_props: EngineSettingsProps): JSX.Element
     key: "engine:SHOW_DATUMS",
     defaultValue: true,
   })
-  // const [enable3D, setEnable3D] = useLocalStorage<boolean>({
-  //   key: "engine:ENABLE_3D",
-  //   defaultValue: false,
-  // })
-  const [perspective3D, setPerspective3D] = useLocalStorage<boolean>({
-    key: "engine:PERSPECTIVE_3D",
-    defaultValue: false,
-  })
 
   async function getEngineSettings(): Promise<void> {
     const settings = await renderer.engine.interface.read_engine_settings()
@@ -47,10 +39,8 @@ export default function EngineSettings(_props: EngineSettingsProps): JSX.Element
       COLOR_BLEND: colorBlend,
       ZOOM_TO_CURSOR: zoomToCursor,
       SHOW_DATUMS: showDatums,
-      // ENABLE_3D: enable3D,
-      PERSPECTIVE_3D: perspective3D,
     })
-  }, [colorBlend, zoomToCursor, showDatums, perspective3D])
+  }, [colorBlend, zoomToCursor, showDatums])
 
   useEffect(() => {
     actions.push({
@@ -106,11 +96,6 @@ export default function EngineSettings(_props: EngineSettingsProps): JSX.Element
       <Flex align="center" style={{ width: "100%" }} justify="space-between">
         <Text>Show Datums</Text>
         <Switch checked={showDatums} onChange={(event): void => setShowDatums(event.currentTarget.checked)} />
-      </Flex>
-      <Divider my="sm" />
-      <Flex align="center" style={{ width: "100%" }} justify="space-between">
-        <Text>3D Perspective View</Text>
-        <Switch checked={perspective3D} onChange={(event): void => setPerspective3D(event.currentTarget.checked)} />
       </Flex>
     </>
   )
